3716b9e404cb3236494e2bce5d62b65fcbf0898d
mariagabrielaromero <romero.maria@correo.unimet.edu.ve>
2024-08-08 17:49:24 -0400

    Subidas las opciones de graficos, estadistica y buscar personaje

--- Personaje.py
+++ Personaje.py
@@ -1,18 +1,20 @@
 class Personaje:
-    def __init__(self, nombre, planeta_origen, episodios, genero, especie, nombre_nave, nombre_vehiculo):
+    def __init__(self, nombre, planeta_origen, genero, nombre_nave, episodios, nombre_vehiculo, especie):
         self.nombre=nombre
         self.planeta_origen=planeta_origen
-        self.episodios=episodios
         self.genero=genero
-        self.especie=especie
-        self.nombre_nave=[]
-        self.nombre_vehiculo=[]
+        self.nombre_nave=nombre_nave
+        self.episodios=episodios
+        self.nombre_vehiculo= nombre_vehiculo
+        self.especie= especie
+
 
     def show(self):
         print(f"El nombre del personaje es: {self.nombre}")
         print(f"El plantea de origen es: {self.planeta_origen}")
-        print(f"Los episodios que interviene son: {self.episodios}")
         print(f"Su genero es: {self.genero}")
-        print(f"Su especie es: {self.especie}")
         print(f"El nombre de las naves que utiliza son: {self.nombre_nave}")
+        print(f"Los episodios en los que aparece son: {self.episodios}")
         print(f"El nombre de los vehiculos que utiliza son: {self.nombre_vehiculo}")
+        print(f"Su especie es: {self.especie}")
+        
